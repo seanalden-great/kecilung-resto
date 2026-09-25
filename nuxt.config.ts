f95fadfca78846c15227e6d1a049894cf2f9b161
Sean Alden <c14210052@john.petra.ac.id>
2026-09-25 13:16:21 +0700

    update nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,33 @@
+// // https://nuxt.com/docs/api/configuration/nuxt-config
+// export default defineNuxtConfig({
+//   modules: ['@nuxtjs/tailwindcss'],
+//   // css: ['./app/assets/css/tailwind.css'],
+// })
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: ['@nuxtjs/tailwindcss'],
-  // css: ['./app/assets/css/tailwind.css'],
+  
+  // Konfigurasi Head Global (Tab Title, Favicon, Meta Tag)
+  app: {
+    head: {
+      title: 'The Kecilung Kitchen & Resto',
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { 
+          name: 'description', 
+          content: 'The Kecilung Kitchen & Resto - Nikmati hidangan istimewa dan suasana nyaman di Surabaya.' 
+        }
+      ],
+      link: [
+        { 
+          rel: 'icon', 
+          type: 'image/webp', 
+          // Pastikan gambar kecilung_logo.webp benar-benar ada di dalam folder: public/assets/images/
+          href: '/assets/images/kecilung_logo.webp' 
+        }
+      ]
+    }
+  }
 })
